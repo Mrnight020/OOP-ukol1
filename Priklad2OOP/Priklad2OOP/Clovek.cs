398f2cbf4165174940b1c578c3cb012e9ceec819
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2022-11-27 11:49:39 +0100

    Add files via upload

---
+++ Priklad2OOP/Priklad2OOP/Clovek.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Priklad2OOP
+{
+    class Clovek
+    {
+        string jmeno;
+        string narozen;
+
+        public Clovek(string jmeno,string narozen)
+        {
+            this.jmeno = jmeno;
+            this.narozen = narozen;
+        }
+
+        public void NastavJmeno(string jmeno)
+        {
+            this.jmeno = jmeno;
+        }
+        public string VypisJmeno()
+        {
+            return jmeno;
+        }
+        public double Vek()
+        {
+            DateTime dnes = DateTime.Now;
+            DateTime narozeni = DateTime.Parse(narozen);
+            TimeSpan vek = dnes - narozeni;
+            
+            return (vek.TotalDays / 365);
+        }
+        public bool Plnolety()
+        {
+            double vek = Vek();
+            if(vek < 18)return false;
+            else return true;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} má {1} let a {2}", VypisJmeno(), (int)Vek(), Plnolety()? "je plnoletý" : "není plnoletý");
+        }
+        public string Starsi(Clovek clovek1,Clovek clovek2)
+        {            
+            double vek1 = clovek1.Vek();
+            double vek2 = clovek2.Vek();
+            if(vek1 > vek2) {
+                return "starsi je " + clovek1.jmeno;
+            }else{
+                return "starsi je " + clovek2.jmeno;
+            }
+        }
+
+
+    }
+}
---
+++ Priklad2OOP/Priklad2OOP/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Priklad2OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clovek clovicek1 = new Clovek(textBox1.Text, maskedTextBox1.Text);
+                Clovek clovicek2 = new Clovek(textBox2.Text, maskedTextBox2.Text);
+                MessageBox.Show("Jmeno 1. cloveka: " + clovicek1.VypisJmeno() + "\n" +
+                                "Jmeno 2. cloveka: " + clovicek2.VypisJmeno());
+
+                clovicek2.NastavJmeno("Pepa");
+                textBox2.Text = "Pepa";
+
+                MessageBox.Show("Jméno : " + clovicek1.VypisJmeno() + "\n\n" +
+                                "Vek :" + ((int)clovicek1.Vek()) + "\n\n" +
+                                String.Format("Je plnolety ? : {0}", clovicek1.Plnolety() ? "Ano" : "Ne") + "\n\n" +
+                                clovicek1.ToString(), "Clovek1");
+
+                MessageBox.Show("Jméno : " + clovicek2.VypisJmeno() + "\n\n" +
+                                "Vek Pepi: " + ((int)clovicek2.Vek()) + "\n\n" +
+                                String.Format("Je plnolety ? : {0}", clovicek2.Plnolety() ? "Ano" : "Ne") + "\n\n" +
+                                clovicek2.ToString(), "Clovek2");
+
+                MessageBox.Show(clovicek1.Starsi(clovicek1, clovicek2));
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+
+        }
+    }
+}
